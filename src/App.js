abb805507c2b158a0d467c388e9ea5c643faadb3
Rohit <srohit1101@gmail.com>
2020-07-11 17:28:46 +0530

    first hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useContext, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+export function App() {
+  const [name, setName] = useState("Rohit");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello from hooks {name}</h1>
     </div>
   );
 }
-
-export default App;
