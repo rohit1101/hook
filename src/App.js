aaa660891741c5a499c1e00d0278d3bad78f37e4
Rohit <srohit1101@gmail.com>
2020-07-11 18:10:49 +0530

    first hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { useState, useContext, useEffect } from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+
 import "./App.css";
 
 export function App() {
   const [name, setName] = useState("Rohit");
 
+  function handleClick() {
+    setName("Batman");
+  }
+
   return (
     <div>
       <h1>Hello from hooks {name}</h1>
+      <button onClick={handleClick}>Click</button>
     </div>
   );
 }
