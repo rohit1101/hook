f11b6947c5ee49097b600ae48653291508212236
Rohit <srohit1101@gmail.com>
2020-07-12 17:08:02 +0530

    rick and morty

--- src/App.js
+++ src/App.js
@@ -3,24 +3,31 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 export function App() {
-  const [name, setName] = useState("Rohit");
+  const [name, setName] = useState(0);
 
-  function handleClick() {
-    getData();
-  }
+  // function handleClick() {}
 
-  useEffect(() => {
-    async function getData() {
-      const res = await fetch("https://rickandmortyapi.com/api/episode");
-      const data = await res.json();
-      setName(data);
-    }
+  useEffect(async () => {
+    const res = await fetch("https://rickandmortyapi.com/api/episode");
+    const data = await res.json();
+    console.log(data.results);
   });
 
   return (
     <div>
-      <h1>Hello from hooks {name}</h1>
-      <button onClick={handleClick}>Click</button>
+      <h1>Hello from {name}</h1>
+      <ul className="pagination">
+        <li className="page-item">
+          <a className="page-link" href="#">
+            Previous
+          </a>
+        </li>
+        <li className="page-item">
+          <a className="page-link" href="#">
+            Next
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
