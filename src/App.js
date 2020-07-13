960a81d1c69206149e7256c59aab20a8cd286ed4
Rohit <srohit1101@gmail.com>
2020-07-13 18:44:59 +0530

    rick and morty

--- src/App.js
+++ src/App.js
@@ -4,40 +4,22 @@ import "./App.css";
 
 export function App() {
   const [currentState, setEpisodes] = useState([]);
-  const [currentImg, setImg] = useState([]);
   const [currentPage, setPage] = useState([]);
 
   useEffect(() => {
     async function getData() {
       const res = await fetch("https://rickandmortyapi.com/api/episode");
       const data = await res.json();
-      console.log(data);
       setEpisodes(data.results);
       setPage(data.info);
     }
 
-    async function getImage() {
-      const res = await fetch("https://rickandmortyapi.com/api/character/1");
-      const data = await res.json();
-      setImg(data.image);
-    }
-
     getData();
-    getImage();
   }, []);
 
   return (
     <div>
       <Card content={currentState} />
-      {currentState.map((item) => {
-        return (
-          <div key={item.id}>
-            <h1>{item.name}</h1>
-            <p>{item.air_date}</p>
-            <p>{item.episode}</p>
-          </div>
-        );
-      })}
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-
+import { Image } from "./Image";
 export function Card({ content }) {
-  return {content.map((item) => {
+  return (
+    <div>
+      {content.map((item) => {
         return (
           <div key={item.id}>
             <h1>{item.name}</h1>
             <p>{item.air_date}</p>
             <p>{item.episode}</p>
           </div>
-}  
+        );
+      })}
+    </div>
+  );
 }
