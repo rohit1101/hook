bcfbbadcd7e25783f46b78b848ab4c93746f5986
Rohit <srohit1101@gmail.com>
2020-07-13 22:04:01 +0530

    rick and morty

--- src/App.js
+++ src/App.js
@@ -13,12 +13,40 @@ export function App() {
       setEpisodes(data.results);
       setPage(data.info);
     }
-
     getData();
   }, []);
 
+  async function handleClick(e) {
+    e.preventDefault();
+    if (currentPage.next) {
+      const res = await fetch(currentPage.next);
+      const data = await res.json();
+      setEpisodes(data.results);
+      setPage(data.info);
+      console.log(data.info);
+    }
+    if (currentPage.prev) {
+      const res = await fetch(currentPage.prev);
+      const data = await res.json();
+      setEpisodes(data.results);
+      setPage(data.info);
+    }
+  }
+
   return (
     <div>
+      <ul className="pagination">
+        <li className="page-item">
+          <a className="page-link" href="#" onClick={handleClick}>
+            Previous
+          </a>
+        </li>
+        <li className="page-item">
+          <a className="page-link" href="#" onClick={handleClick}>
+            Next
+          </a>
+        </li>
+      </ul>
       <Card content={currentState} />
     </div>
   );
