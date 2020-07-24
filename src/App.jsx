791718680181c53b193f79f9d1ed0751927f43e1
rohit1101 <srohi1101@gmail.com>
2020-07-24 22:11:20 +0530

    navbar style

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,113 @@
-import React, { useEffect, useState } from "react";
-import { Card } from "./Card";
+import React, { useEffect, useState } from "react"
+import { Card } from "./Card"
+import AppBar from "@material-ui/core/AppBar"
+import Tabs from "@material-ui/core/Tabs"
+import Button from "@material-ui/core/Button"
+import ButtonGroup from "@material-ui/core/ButtonGroup"
 
-import "./App.css";
+// import Tab from "@material-ui/core/Tab"
+// import Typography from "@material-ui/core/Typography"
+// import Box from "@material-ui/core/Box"
+
+import "./App.css"
 
 export function App() {
-  const [currentState, setEpisodes] = useState([]);
-  const [currentPage, setPage] = useState([]);
-  const [currentValue, setValue] = useState("");
-  const [currentError, setError] = useState("");
-  const [currentLoad, setLoading] = useState(false);
+  const [currentState, setEpisodes] = useState([])
+  const [currentPage, setPage] = useState([])
+  const [currentValue, setValue] = useState("")
+  const [currentError, setError] = useState("")
+  const [currentLoad, setLoading] = useState(false)
 
   async function getData(url = `https://rickandmortyapi.com/api/episode`) {
-    setLoading(true);
-    const res = await fetch(url);
-    const data = await res.json();
+    setLoading(true)
+    const res = await fetch(url)
+    const data = await res.json()
 
     if (data.error) {
-      setError(data.error);
-      setLoading(false);
+      setError(data.error)
+      setLoading(false)
     }
     if (!data.error) {
-      setError("");
-      setEpisodes(data.results);
-      setPage(data.info);
-      setLoading(false);
+      setError("")
+      setEpisodes(data.results)
+      setPage(data.info)
+      setLoading(false)
     }
   }
 
   useEffect(() => {
-    getData();
-  }, []);
+    getData()
+  }, [])
 
   async function handleNextClick(e) {
-    e.preventDefault();
+    e.preventDefault()
     if (currentPage.next) {
-      setLoading(true);
-      const res = await fetch(currentPage.next);
-      const data = await res.json();
-      setEpisodes(data.results);
-      setPage(data.info);
-      setLoading(false);
+      setLoading(true)
+      const res = await fetch(currentPage.next)
+      const data = await res.json()
+      setEpisodes(data.results)
+      setPage(data.info)
+      setLoading(false)
     }
   }
 
   async function handlePrevClick(e) {
-    e.preventDefault();
+    e.preventDefault()
 
     if (currentPage.prev) {
-      setLoading(true);
-      const res = await fetch(currentPage.prev);
-      const data = await res.json();
+      setLoading(true)
+      const res = await fetch(currentPage.prev)
+      const data = await res.json()
 
-      setEpisodes(data.results);
-      setPage(data.info);
-      setLoading(false);
+      setEpisodes(data.results)
+      setPage(data.info)
+      setLoading(false)
     }
   }
 
   async function handleChange(e) {
-    setLoading(true);
-    setValue(e.target.value);
+    setLoading(true)
+    setValue(e.target.value)
     if (currentValue) {
-      getData(
-        `https://rickandmortyapi.com/api/episode/?name=${e.target.value}`
-      );
+      getData(`https://rickandmortyapi.com/api/episode/?name=${e.target.value}`)
     }
     if (currentValue === "") {
-      setLoading(true);
-      getData();
+      setLoading(true)
+      getData()
     }
   }
 
   return (
     <div>
-      <ul className="pagination">
-        <li className="page-item">
-          <a className="page-link" href="#" onClick={handlePrevClick}>
-            Previous
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href="#" onClick={handleNextClick}>
-            Next
-          </a>
-        </li>
-      </ul>
+      <AppBar position="sticky" color="transparent" centered>
+        <Tabs
+          variant="fullWidth"
+          // value={value}
+          // onChange={handleChange}
+          aria-label="nav tabs example"
+        >
+          <ButtonGroup
+            color="transparent"
+            aria-label="outlined primary button group"
+          >
+            <Button variant="outlined">One</Button>
+            <Button variant="outlined">Two</Button>
+            <Button variant="outlined">Three</Button>
+          </ButtonGroup>
+          {/* <LinkTab label="Page One" href="/drafts" {...a11yProps(0)} />
+          <LinkTab label="Page Two" href="/trash" {...a11yProps(1)} />
+          <LinkTab label="Page Three" href="/spam" {...a11yProps(2)} /> */}
+        </Tabs>
+      </AppBar>
+
+      {/* <TabPanel value={value} index={0}>
+        Page One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Page Two
+      </TabPanel> */}
+      {/* <TabPanel>Page Three</TabPanel> */}
+
       <label>
         Search Episode
         <input
@@ -97,5 +120,5 @@ export function App() {
 
       <Card content={currentState} er={currentError} load={currentLoad} />
     </div>
-  );
+  )
 }
